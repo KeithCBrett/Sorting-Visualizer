d5afb95de249997ade2cc1457c910438e50d34e8
KeithCBrett <keithcbrett@gmail.com>
2023-12-09 12:03:45 -0500

    generate_bars
    Created generate_bars function that will generate any number of bars and shuffle them. will then display these bars to the screen. This will be used in the sorting animation and will be the thing being sorted.

--- Visualizer.py
+++ Visualizer.py
@@ -1,10 +1,20 @@
 # Import Matplotlib, this library has built-in functions for creating animations. We will be using it to draw the
 # sorting animation to the screen.
+# We will also import random so that we can mix up are bars so that they can be sorted.
 
 import matplotlib.pyplot
+import random
 
-x = [1, 2, 3, 4, 5]
-y = [1, 2, 3, 4, 5]
 
-matplotlib.pyplot.bar(x, y)
-matplotlib.pyplot.show()
+# This function will generate a random set of bars to be sorted using matplotlib. The number of bars generated will be
+# equal to number_bar
+def generate_bars(number_bars):
+    x = [*range(1, (number_bars + 1), 1)]
+    y = [*range(1, (number_bars + 1), 1)]
+    random.shuffle(x)
+    random.shuffle(y)
+    matplotlib.pyplot.bar(x, y)
+    matplotlib.pyplot.show()
+
+
+generate_bars(10)
