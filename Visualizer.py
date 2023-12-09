4171d79523a9195aa667d783b67589cbab7497b1
KeithCBrett <keithcbrett@gmail.com>
2023-12-09 10:30:55 -0500

    Update Visualizer.py
    Test of matplot lib. Renders a ordered bar plot based off of x and y. This could be used to display what is being ordered.

--- Visualizer.py
+++ Visualizer.py
@@ -1,2 +1,10 @@
 # Import Matplotlib, this library has built-in functions for creating animations. We will be using it to draw the
 # sorting animation to the screen.
+
+import matplotlib.pyplot
+
+x = [1, 2, 3, 4, 5]
+y = [1, 2, 3, 4, 5]
+
+matplotlib.pyplot.bar(x, y)
+matplotlib.pyplot.show()
